171ef2b9b463069d3a46ef57973a4808d955b1ea
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-08-19 19:12:24 +0530

    Rotate Array.py

--- Arrays/Rotate Array.py
+++ Arrays/Rotate Array.py
@@ -31,3 +31,22 @@ Follow up:
 Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
 Could you do it in-place with O(1) extra space?
 Solution:'''
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        l,r=0,len(nums)-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r=l+1,r-1
+        l,r=0,k-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r=l+1,r-1
+        l,r=k,len(nums)-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r=l+1,r-1
