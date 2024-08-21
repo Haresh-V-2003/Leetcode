6a69152a66b54cdb572be9b302cd5af722b0d70c
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-08-21 20:38:08 +0530

    Longest Common Prefix.py

--- Arrays/Longest Common Prefix.py
+++ Arrays/Longest Common Prefix.py
@@ -1,4 +1,4 @@
-Write a function to find the longest common prefix string amongst an array of strings.
+'''Write a function to find the longest common prefix string amongst an array of strings.
 
 If there is no common prefix, return an empty string "".
 
@@ -20,3 +20,18 @@ Constraints:
 1 <= strs.length <= 200
 0 <= strs[i].length <= 200
 strs[i] consists of only lowercase English letters.
+
+Solution:'''
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        min_len=float('inf')
+        for s in strs:
+            if len(s)<min_len:
+                min_len=len(s)
+        i=0
+        while i<min_len:
+            for s in strs:
+                if s[i]!=strs[0][i]:
+                    return s[:i]
+            i+=1
+        return s[:i]
