d76d82f7e3bf5c2cab89e526d91c1b76d18ff8be
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-08-28 17:00:42 +0530

    Product of Array Except Self.py

--- Arrays/Product of Array Except Self.py
+++ Arrays/Product of Array Except Self.py
@@ -1,4 +1,4 @@
-Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+'''Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 
@@ -21,3 +21,19 @@ Constraints:
 2 <= nums.length <= 105
 -30 <= nums[i] <= 30
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+Solution:'''
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left_mul=1
+        right_mul=1
+        n=len(nums)
+        left_arr=[0]*n
+        right_arr=[0]*n
+        for i in range(n):
+            j=-i-1
+            left_arr[i]=left_mul
+            right_arr[j]=right_mul
+            left_mul*=nums[i]
+            right_mul*=nums[j]
+        return [l*r for l,r in zip(left_arr,right_arr)]
