13aa65749551a804e54666ea758f0b3e9f068266
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-08-30 22:36:58 +0530

    Merge Intervals.py

--- Arrays/Merge Intervals.py
+++ Arrays/Merge Intervals.py
@@ -1,4 +1,4 @@
-Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+'''Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
  
 
@@ -19,3 +19,15 @@ Constraints:
 1 <= intervals.length <= 104
 intervals[i].length == 2
 0 <= starti <= endi <= 104
+
+Solution:'''
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda interval:interval[0])
+        merged=[]
+        for interval in intervals:
+            if not merged or merged[-1][1]<interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1]=[merged[-1][0],max(merged[-1][1],interval[1])]
+        return merged
