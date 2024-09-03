7a2090b6f2fd608fbd125c85301faf6eeca7006f
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-03 14:11:05 +0530

    Rotate Image.py

--- Arrays/Rotate Image.py
+++ Arrays/Rotate Image.py
@@ -1,4 +1,4 @@
-You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+'''You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
 
 You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 
@@ -21,3 +21,19 @@ Constraints:
 n == matrix.length == matrix[i].length
 1 <= n <= 20
 -1000 <= matrix[i][j] <= 1000
+
+Solution:'''
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n=len(matrix)
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+        
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j],matrix[i][n-j-1]=matrix[i][n-j-1],matrix[i][j]
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
