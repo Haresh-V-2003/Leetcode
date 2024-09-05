d9452dac009549d2e10511d8e2b444687cfa1a12
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-05 10:01:33 +0530

    Jewels and Stones.py

--- Hashmaps ans Sets/Jewels and Stones.py
+++ Hashmaps ans Sets/Jewels and Stones.py
@@ -1,4 +1,4 @@
-You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+'''You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
 
 Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
@@ -19,3 +19,13 @@ Constraints:
 1 <= jewels.length, stones.length <= 50
 jewels and stones consist of only English letters.
 All the characters of jewels are unique.
+
+Solution:'''
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        s=set(jewels)
+        count=0
+        for stone in stones:
+            if stone in s:
+                count+=1
+        return count
