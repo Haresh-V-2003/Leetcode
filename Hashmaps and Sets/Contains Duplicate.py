a7d3ad045eb8ad527eb4e335b02f1a3a8414f2c0
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-06 12:03:27 +0530

    Contains Duplicate.py

--- Hashmaps and Sets/Contains Duplicate.py
+++ Hashmaps and Sets/Contains Duplicate.py
@@ -1,4 +1,4 @@
-Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+'''Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
  
 
@@ -28,8 +28,16 @@ Input: nums = [1,1,1,3,3,4,3,2,4,2]
 
 Output: true
 
- 
-
+Solution:''' 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+       h= set()
+       for num in nums:
+        if num in h:
+            return True
+        else:
+            h.add(num)
+       return False
 Constraints:
 
 1 <= nums.length <= 105
