16e7d55f11f4cd6b0c864f952abfa3b4fed993bc
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-07 13:39:44 +0530

    Ransom Note.py

--- Hashmaps and Sets/Ransom Note.py
+++ Hashmaps and Sets/Ransom Note.py
@@ -1,4 +1,4 @@
-Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+'''Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
 
 Each letter in magazine can only be used once in ransomNote.
 
@@ -22,3 +22,21 @@ Constraints:
 
 1 <= ransomNote.length, magazine.length <= 105
 ransomNote and magazine consist of lowercase English letters.
+
+Solution:'''
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter={}
+        for m in magazine:
+            if m in counter:
+                counter[m]+=1
+            else:
+                counter[m]=1
+        for r in ransomNote:
+            if r not in counter:
+                return False
+            elif counter[r]==1:
+                del counter[r]
+            else:
+                counter[r]-=1
+        return True
