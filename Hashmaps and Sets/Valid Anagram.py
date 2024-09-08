b22a87cb02d01da3e540d0de58f3839c2d140b67
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-08 11:00:46 +0530

    Valid Anagram.py

--- Hashmaps and Sets/Valid Anagram.py
+++ Hashmaps and Sets/Valid Anagram.py
@@ -1,4 +1,4 @@
-Given two strings s and t, return true if t is an 
+'''Given two strings s and t, return true if t is an 
 anagram
  of s, and false otherwise.
 
@@ -22,3 +22,23 @@ Constraints:
 
 1 <= s.length, t.length <= 5 * 104
 s and t consist of lowercase English letters.
+
+Solution:'''
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counter={}
+        if len(s)!=len(t):
+            return False
+        for a in s:
+            if a in counter:
+                counter[a]+=1
+            else:
+                counter[a]=1
+        for b in t:
+            if b not in counter:
+                return False
+            elif counter[b]==1:
+                del counter[b]
+            else:
+                counter[b]-=1
+        return True
