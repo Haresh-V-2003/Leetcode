ca1ffede8bf932dd3c41435da1efc7cd16063d52
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-09 19:44:27 +0530

    Maximum Number of Balloons.py

--- Hashmaps and Sets/Maximum Number of Balloons.py
+++ Hashmaps and Sets/Maximum Number of Balloons.py
@@ -1,4 +1,4 @@
-Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+'''Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
 
 You can use each character in text at most once. Return the maximum number of instances that can be formed.
 
@@ -26,3 +26,16 @@ Constraints:
 
 1 <= text.length <= 104
 text consists of lower case English letters only.
+
+Solution:'''
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        counter=defaultdict(int)
+        balloon='balloon'
+        for s in text:
+            if s in balloon:
+                counter[s]+=1
+        for s in balloon:
+            if s not in counter:
+                return 0
+        return min(counter['b'],counter['a'],counter['l']//2,counter['o']//2,counter['n'])
