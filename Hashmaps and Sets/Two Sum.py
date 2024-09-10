9bf9230acd87369b8843aed429b96f6f6d4360c5
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-10 22:49:53 +0530

    Two Sum.py

--- Hashmaps and Sets/Two Sum.py
+++ Hashmaps and Sets/Two Sum.py
@@ -1,4 +1,4 @@
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+'''Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
@@ -27,3 +27,14 @@ Constraints:
 -109 <= nums[i] <= 109
 -109 <= target <= 109
 Only one valid answer exists.
+
+Solution:'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h={}
+        for i in range(len(nums)):
+            h[nums[i]]=i
+        for i in range(len(nums)):
+            y=target-nums[i]
+            if y in h and h[y]!=i:
+                return [i,h[y]]
