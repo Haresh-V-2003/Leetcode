57b35f810a22ddb657b5f4e873f656c777ab588e
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-11 18:53:18 +0530

    Valid Sudoku.py

--- Hashmaps and Sets/Valid Sudoku.py
+++ Hashmaps and Sets/Valid Sudoku.py
@@ -1,4 +1,4 @@
-Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+'''Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 
 Each row must contain the digits 1-9 without repetition.
 Each column must contain the digits 1-9 without repetition.
@@ -44,3 +44,41 @@ Constraints:
 board.length == 9
 board[i].length == 9
 board[i][j] is a digit 1-9 or '.'.
+
+Solution:'''
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #validate rows
+        for i in range(9):
+            s=set()
+            for j in range(9):
+                item=board[i][j]
+                if item in s:
+                    return False
+                elif item !='.':
+                    s.add(item)
+
+        #validate cols
+        for i in range(9):
+            s=set()
+            for j in range(9):
+                item=board[j][i]
+                if item in s:
+                    return False
+                elif item !='.':
+                    s.add(item)
+
+        #validate grids
+        starts=[(0,0),(0,3),(0,6),
+        (3,0),(3,3),(3,6),
+        (6,0),(6,3),(6,6)]
+        for i,j in starts:
+            s=set()
+            for rows in range(i,i+3):
+                for cols in range(j,j+3):
+                    item=board[rows][cols]
+                    if item in s:
+                        return False
+                    elif item !='.':
+                        s.add(item)
+        return True
