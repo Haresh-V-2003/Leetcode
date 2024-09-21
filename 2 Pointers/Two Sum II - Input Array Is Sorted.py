1c65a897be1754489f482587f7b3720bbd0a1edd
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-21 23:06:49 +0530

    Two Sum II - Input Array Is Sorted.py

--- 2 Pointers/Two Sum II - Input Array Is Sorted.py
+++ 2 Pointers/Two Sum II - Input Array Is Sorted.py
@@ -1,4 +1,4 @@
-Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+'''Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
 
 Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
 
@@ -32,3 +32,18 @@ Constraints:
 numbers is sorted in non-decreasing order.
 -1000 <= target <= 1000
 The tests are generated such that there is exactly one solution.
+
+Solution:'''
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l=0
+        n=len(numbers)
+        r=n-1
+        while l<r:
+            summ=numbers[l]+numbers[r]
+            if summ==target:
+                return [l+1,r+1]
+            elif summ<target:
+                l+=1
+            else:
+                r-=1
