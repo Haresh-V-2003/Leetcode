3c7bc110834d0fdd2b8abf6ae618eb4a76e05888
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-22 20:30:45 +0530

    Valid Palindrome.py

--- 2 Pointers/Valid Palindrome.py
+++ 2 Pointers/Valid Palindrome.py
@@ -1,4 +1,4 @@
-A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+'''A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 
 Given a string s, return true if it is a palindrome, or false otherwise.
 
@@ -26,3 +26,22 @@ Constraints:
 
 1 <= s.length <= 2 * 105
 s consists only of printable ASCII characters.
+
+Solution:'''
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n=len(s)
+        l=0
+        r=n-1
+        while l<r:
+            if not s[l].isalnum():
+                l+=1
+                continue #use continue to avoid executing other conditions
+            if not s[r].isalnum():
+                r-=1
+                continue
+            if s[l].lower()!=s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
