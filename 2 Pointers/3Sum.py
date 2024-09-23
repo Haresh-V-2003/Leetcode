9a38e4e4c25a614639ffc3e60ebc9a1c3a7b5bc5
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-23 22:13:40 +0530

    3Sum.py

--- 2 Pointers/3Sum.py
+++ 2 Pointers/3Sum.py
@@ -1,4 +1,4 @@
-Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+'''Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
 Notice that the solution set must not contain duplicate triplets.
 
@@ -30,3 +30,31 @@ Constraints:
 
 3 <= nums.length <= 3000
 -105 <= nums[i] <= 105
+
+Solution:'''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans=[]
+        n=len(nums)
+        for i in range(n):
+            if nums[i]>0:
+                break
+            elif i>0 and nums[i]==nums[i-1]:
+                continue
+            lo,hi=i+1,n-1
+            while lo<hi:
+                summ=nums[i]+nums[lo]+nums[hi]
+                if summ==0:
+                    ans.append([nums[i],nums[lo],nums[hi]])
+                    lo+=1
+                    hi-=1
+                    while lo<hi and nums[lo]==nums[lo-1]:
+                        lo+=1
+                    while lo<hi and nums[hi]==nums[hi+1]:
+                        hi-=1
+                elif summ<0:
+                    lo+=1
+                else:
+                    hi-=1
+        return ans
