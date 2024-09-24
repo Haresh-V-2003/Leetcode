fa31126940676a0abb2e901fd02e1f3bd50d9517
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-24 21:52:41 +0530

    Container With Most Water.py

--- 2 Pointers/Container With Most Water.py
+++ 2 Pointers/Container With Most Water.py
@@ -1,4 +1,4 @@
-You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+'''You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 
 Find two lines that together with the x-axis form a container, such that the container contains the most water.
 
@@ -25,3 +25,22 @@ Constraints:
 n == height.length
 2 <= n <= 105
 0 <= height[i] <= 104
+
+Solution:'''
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n=len(height)
+        l=0
+        r=n-1
+        maxarea=0
+        while l<r:
+            w=r-l
+            h=min(height[l],height[r])
+            area=w*h
+            if area>maxarea:
+                maxarea=area
+            if height[l]<height[r]:
+                l+=1
+            else:
+                r-=1
+        return maxarea
