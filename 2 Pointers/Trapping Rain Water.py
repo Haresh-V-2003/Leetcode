74094435c063456f12ffa6de7e090a2f883600a5
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-09-30 08:41:45 +0530

    Trapping Rain Water.py

--- 2 Pointers/Trapping Rain Water.py
+++ 2 Pointers/Trapping Rain Water.py
@@ -1,4 +1,4 @@
-Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+'''Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
  
 
@@ -19,3 +19,22 @@ Constraints:
 n == height.length
 1 <= n <= 2 * 104
 0 <= height[i] <= 105
+
+Solution:'''
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n=len(height)
+        l_wall=r_wall=0
+        max_left=[0]*n
+        max_right=[0]*n
+        for i in range(n):
+            j=-i-1
+            max_left[i]=l_wall
+            max_right[j]=r_wall
+            l_wall=max(l_wall,height[i])
+            r_wall=max(r_wall,height[j])
+        summ=0
+        for i in range(n):
+            pot=min(max_left[i],max_right[i])
+            summ+=max(0,pot-height[i])
+        return summ
