25447d4bf182fded84d3346282f1e1c2ace8376b
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-10-11 22:31:40 +0530

    Reverse Linked List.py

--- Linked Lists/Reverse Linked List.py
+++ Linked Lists/Reverse Linked List.py
@@ -1,4 +1,4 @@
-Given the head of a singly linked list, reverse the list, and return the reversed list.
+'''Given the head of a singly linked list, reverse the list, and return the reversed list.
 
  
 
@@ -22,3 +22,20 @@ Constraints:
 
 The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000
+
+Solution:'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev=None
+        cur=head
+        while cur:
+            t=cur.next
+            cur.next=prev
+            prev=cur
+            cur=t
+        return prev
