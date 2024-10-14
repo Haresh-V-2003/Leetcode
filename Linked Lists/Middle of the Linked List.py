ee743b42d08a048cd71ecdcf0c93e62231db894f
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-10-14 22:51:47 +0530

    Middle of the Linked List.py

--- Linked Lists/Middle of the Linked List.py
+++ Linked Lists/Middle of the Linked List.py
@@ -1,4 +1,4 @@
-Given the head of a singly linked list, return the middle node of the linked list.
+'''Given the head of a singly linked list, return the middle node of the linked list.
 
 If there are two middle nodes, return the second middle node.
 
@@ -22,3 +22,17 @@ Constraints:
 
 The number of nodes in the list is in the range [1, 100].
 1 <= Node.val <= 100
+
+Solution:'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow=fast=head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+        return slow
