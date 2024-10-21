b8deca05f9575c63b1c8f8e7d996ae4f1af4b769
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-10-21 21:53:32 +0530

    Valid Perfect Square.py

--- Binary Search/Valid Perfect Square.py
+++ Binary Search/Valid Perfect Square.py
@@ -1,4 +1,4 @@
-Given a positive integer num, return true if num is a perfect square or false otherwise.
+'''Given a positive integer num, return true if num is a perfect square or false otherwise.
 
 A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
 
@@ -21,3 +21,18 @@ Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an inte
 Constraints:
 
 1 <= num <= 231 - 1
+
+Solution:'''
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        l=1
+        r=num
+        while l<=r:
+            m=(l+r)//2
+            if m*m<num:
+                l=m+1
+            elif m*m>num:
+                r=m-1
+            else:
+                return True
+        return False
