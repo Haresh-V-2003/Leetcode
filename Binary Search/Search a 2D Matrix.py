b95cced3714c6f1d00b99f582fc19fe551ba9cf2
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-10-22 22:28:10 +0530

    Search a 2D Matrix.py

--- Binary Search/Search a 2D Matrix.py
+++ Binary Search/Search a 2D Matrix.py
@@ -1,4 +1,4 @@
-You are given an m x n integer matrix matrix with the following two properties:
+'''You are given an m x n integer matrix matrix with the following two properties:
 
 Each row is sorted in non-decreasing order.
 The first integer of each row is greater than the last integer of the previous row.
@@ -26,3 +26,23 @@ m == matrix.length
 n == matrix[i].length
 1 <= m, n <= 100
 -104 <= matrix[i][j], target <= 104
+
+Solution:'''
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows=len(matrix)
+        cols=len(matrix[0])
+        l=0
+        r=(rows*cols)-1
+        while l<=r:
+            mid=(l+r)//2
+            i=mid//cols
+            j=mid%cols
+            #i,j are used to find the row,col index of the mid element using formulas
+            if matrix[i][j]<target:
+                l=mid+1
+            elif matrix[i][j]>target:
+                r=mid-1
+            else:
+                return True
+        return False
