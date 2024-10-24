b3c23c6cafa3920d5f7f95b24a2a225c01d587ad
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-10-24 21:13:58 +0530

    Search in Rotated Sorted Array.py

--- Binary Search/Search in Rotated Sorted Array.py
+++ Binary Search/Search in Rotated Sorted Array.py
@@ -1,4 +1,4 @@
-There is an integer array nums sorted in ascending order (with distinct values).
+'''There is an integer array nums sorted in ascending order (with distinct values).
 
 Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
 
@@ -29,3 +29,19 @@ Constraints:
 All values of nums are unique.
 nums is an ascending array that is possibly rotated.
 -104 <= target <= 104
+
+Solution:'''
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        n=len(nums)
+        l=0
+        r=n-1
+        #here we dont use l<=r but use l<r so that the minimum can be found when l==r
+        while l<r:
+            m=(l+r)//2
+            if nums[m]>nums[r]:
+                l=m+1
+            else:
+                r=m
+        #both nums[l] and nums[r] have the same element so we return any one
+        return nums[l]
