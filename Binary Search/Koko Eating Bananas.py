0e8dbea7113f1c26d43b3abc57d764be919cd701
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-10-25 21:56:31 +0530

    Koko Eating Bananas.py

--- Binary Search/Koko Eating Bananas.py
+++ Binary Search/Koko Eating Bananas.py
@@ -1,4 +1,4 @@
-Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+'''Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
 
 Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
 
@@ -27,3 +27,21 @@ Constraints:
 1 <= piles.length <= 104
 piles.length <= h <= 109
 1 <= piles[i] <= 109
+
+Solution:'''
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def k_works(k):
+            hours=0
+            for p in piles:
+                hours+=ceil(p/k)
+            return hours<=h
+        l=1
+        r=max(piles)
+        while l<r:
+            k=(l+r)//2
+            if k_works(k):
+                r=k
+            else:
+                l=k+1
+        return l
