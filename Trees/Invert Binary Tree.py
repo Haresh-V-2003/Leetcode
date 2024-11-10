2cba5bc77e6bec26b6089e5fe91a94344310593c
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-11-10 18:20:16 +0530

    Invert Binary Tree.py

--- Trees/Invert Binary Tree.py
+++ Trees/Invert Binary Tree.py
@@ -1,4 +1,4 @@
-Given the root of a binary tree, invert the tree, and return its root.
+'''Given the root of a binary tree, invert the tree, and return its root.
 
  
 
@@ -22,3 +22,19 @@ Constraints:
 
 The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
+
+Solution:'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left,root.right=root.right,root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
