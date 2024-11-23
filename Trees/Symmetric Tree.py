65355926ec024a9c55e7b9258efab0d67c8a17a3
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-11-23 23:02:52 +0530

    Symmetric Tree.py

--- Trees/Symmetric Tree.py
+++ Trees/Symmetric Tree.py
@@ -1,4 +1,4 @@
-Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+'''Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
  
 
@@ -18,3 +18,22 @@ Constraints:
 
 The number of nodes in the tree is in the range [1, 1000].
 -100 <= Node.val <= 100
+
+Solution:'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def same(root1,root2):
+            if not root1 and not root2:
+                return True
+            if not root1 or not root2:
+                return False
+            if root1.val!=root2.val:
+                return False
+            return same(root1.left,root2.right) and same(root1.right,root2.left)
+        return same(root,root)
