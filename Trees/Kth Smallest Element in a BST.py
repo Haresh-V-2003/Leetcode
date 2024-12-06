75083b04915edbed59ae59a85b9d0e7c86a768c6
Haresh-V-2003 <130588197+Haresh-V-2003@users.noreply.github.com>
2024-12-06 21:14:04 +0530

    Kth Smallest Element in a BST.py

--- Trees/Kth Smallest Element in a BST.py
+++ Trees/Kth Smallest Element in a BST.py
@@ -1,4 +1,4 @@
-Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+'''Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
 
  
 
@@ -19,3 +19,26 @@ Constraints:
 The number of nodes in the tree is n.
 1 <= k <= n <= 104
 0 <= Node.val <= 104
+
+Solution:'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        count=[k]
+        ans=[0]
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            if count[0]==1:
+                ans[0]=node.val
+            count[0]=count[0]-1
+            if count[0]>0:
+                dfs(node.right)
+        dfs(root)
+        return ans[0]
