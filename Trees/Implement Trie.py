1227b33643c92e1085acfb678dd16500804c1599
Haresh Venkatesan <130588197+Haresh-V-2003@users.noreply.github.com>
2024-12-23 19:29:05 +0530

    Update Implement Trie.py

--- Trees/Implement Trie.py
+++ Trees/Implement Trie.py
@@ -33,3 +33,39 @@ word and prefix consist only of lowercase English letters.
 At most 3 * 104 calls in total will be made to insert, search, and startsWith.
 
 Solution:'''
+class Trie:
+
+    def __init__(self):
+        self.trie={}
+
+    def insert(self, word: str) -> None:
+        d= self.trie
+        for c in word:
+            if c not in d:
+                d[c]={}
+            d=d[c]
+        d['.']='.'
+
+    def search(self, word: str) -> bool:
+        d=self.trie
+        for c in word:
+            if c not in d:
+                return False
+            d=d[c]
+        return '.' in d
+
+
+    def startsWith(self, prefix: str) -> bool:
+        d=self.trie
+        for c in prefix:
+            if c not in d:
+                return False
+            d=d[c]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
