2f247e309236975de996befa88c567e92e30c50e
Haresh Venkatesan <130588197+Haresh-V-2003@users.noreply.github.com>
2025-01-11 22:19:05 +0530

    Update Top K Frequent Elements.py

--- Heaps/Top K Frequent Elements.py
+++ Heaps/Top K Frequent Elements.py
@@ -1,4 +1,4 @@
-Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+'''Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 
  
 
@@ -18,3 +18,5 @@ Constraints:
 -104 <= nums[i] <= 104
 k is in the range [1, the number of unique elements in the array].
 It is guaranteed that the answer is unique.
+
+Solution:'''
