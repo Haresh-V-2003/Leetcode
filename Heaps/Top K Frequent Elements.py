2855e0864d134de537f49dce00615212966669bd
Haresh Venkatesan <130588197+Haresh-V-2003@users.noreply.github.com>
2025-01-15 18:36:34 +0530

    Update Top K Frequent Elements.py

--- Heaps/Top K Frequent Elements.py
+++ Heaps/Top K Frequent Elements.py
@@ -20,3 +20,18 @@ k is in the range [1, the number of unique elements in the array].
 It is guaranteed that the answer is unique.
 
 Solution:'''
+# Heap Solution:
+from collections import Counter
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        heap = []
+
+        for key, val in counter.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (val, key))
+            else:
+                heapq.heappushpop(heap, (val, key))
+        
+        return [h[1] for h in heap]
